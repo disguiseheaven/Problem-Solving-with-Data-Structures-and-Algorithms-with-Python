bd070d4c4cc22f4445c39292053deba11fb305b7
Rubi Singh <147939475+disguiseheaven@users.noreply.github.com>
2023-10-18 14:21:59 +0530

    Update D03P3_LeetCode_1486.py

--- Python Solutions/D03P3_LeetCode_1486.py
+++ Python Solutions/D03P3_LeetCode_1486.py
@@ -1,10 +1,5 @@
-# Python3 code coming soon
-
-
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
-        ans = temp = start
-        for i in range(1, n):
-            temp += 2  
-            ans = ans ^ temp
-        return ans
+        nums = [start+2*i for i in range(n)]
+        return (reduce(lambda x,y: x^y, nums))
+        
