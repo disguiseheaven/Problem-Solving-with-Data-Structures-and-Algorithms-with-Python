b8aee440d39d65df9b031fed0135ebd9cf2f6ae3
Rubi Singh <147939475+disguiseheaven@users.noreply.github.com>
2023-10-18 15:41:46 +0530

    Update D05P2_LeetCode_338.py

--- Python Solutions/D05P2_LeetCode_338.py
+++ Python Solutions/D05P2_LeetCode_338.py
@@ -1,7 +1,6 @@
 class Solution:
-    def countBits(self, num: int) -> List[int]:
-        l=[]
-        for i in range(num+1):
-            x=bin(i)
-            l.append(x.count('1'))
-        return l
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)
+        for i in range(1, n + 1):
+            ans[i] = ans[i >> 1] + (i & 1)
+        return ans
